a5d3aaf245a5d874d299c4246ad31e7430adc743
halv <viethahn94@gmail.com>
2020-07-23 14:36:45 +0700

    tao hinh vuong + tao mang 3x3

---
+++ gameOfLifeSelf.js
@@ -0,0 +1,14 @@
+var lifes = [];
+
+lifes.push([1,0,0]);
+lifes.push([0,1,0]);
+lifes.push([1,1,1]);
+
+console.log(lifes);
+
+HTML =[];
+for (let i = 0; i < 3; i++){
+    for (let j = 0; j < 3;j++){
+        HTML = bra
+    }
+}
