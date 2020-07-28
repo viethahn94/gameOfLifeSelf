0dd10865b66ca8422654a74804954d2677bada51
Hoang Phan Trung Hieu <hoangphantrunghieu@Hoangs-MacBook-Pro.local>
2020-07-28 17:46:21 +0700

    finish

--- gameOfLifeSelf.js
+++ gameOfLifeSelf.js
@@ -1,70 +1,92 @@
-var lifes = [];
+const DEATH_CELL = `<div style="width: 10px ; height: 10px ; background: white; border: 1px solid gray; float: left" ></div>`;
+const LIVE_CELL = `<div style="width: 10px ; height: 10px ; background: black; border: 1px solid gray; float: left" ></div>`;
+const LIVE = 1;
+const DEATH = 0;
 
-lifes.push([1,0,0,1,0,0,1,1,0,1]);
-lifes.push([0,1,0,1,0,1,1,0,0,1]);
-lifes.push([1,1,1,0,1,0,0,0,1,1]);
-lifes.push([1,0,0,1,1,1,1,1,1,1]);
-lifes.push([1,0,0,0,0,1,1,0,0,1]);
-lifes.push([1,0,0,1,0,0,1,0,0,1]);
-lifes.push([0,1,0,1,0,1,1,1,0,1]);
-lifes.push([1,1,1,0,1,0,0,1,1,1]);
-lifes.push([1,0,0,1,1,1,1,1,1,1]);
-lifes.push([1,0,0,0,0,1,1,0,0,1]);
-
-
-
-let html = '';
-
-for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-        if (lifes[i][j] == 0){
-            // viết vài dòng vào đây để return cái con củ lìn div màu trắng ở thẻ file html nhưng đm nó éo biết làm T_T
-            html+=`<div style="width: 10px ; height: 10px ; background: white; border: 1px solid gray; float: left" ></div>`;
-        }
-        else {
-            // viết vài dòng vào đây để return cái con củ lìn div màu đen ở thẻ file html nhưng đm nó éo biết làm T_T
-            html+=`<div style="width: 10px ; height: 10px ; background: black; border: 1px solid gray; float: left" ></div>`;
-        }
-    }
+function setupLifeData() {
+    let life = [];
+    life.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    life.push([0, 0, 0, 1, 0, 0, 0, 0, 0, 0]);
+    life.push([0, 1, 0, 1, 0, 0, 0, 0, 0, 0]);
+    life.push([0, 0, 1, 1, 0, 0, 0, 0, 0, 0]);
+    life.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    life.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    life.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    life.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    life.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    life.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    return life;
 }
 
-function draw() {
-    let next = lifes;
-    for (let i = 0; i < lifes.length; i++) {
-        for (let j = 0; j < lifes[0].length; j++) {
-
-            if (i == 0 || i == lifes.length -1 ||j == 0 || j == lifes[0].length -1 ){
-                next[i][j] = lifes[i][j];
-            }
-            else {
-            let state = lifes[i][j];
-            let neighbors = countLiveNeighbors(lifes, i, j);
-                if (state == 0 && neighbors == 3) {
-                next[i][j] =1;
-                } else if(state == 1 && (neighbors > 3 || neighbors < 2)){
-                next[i][j] =0;
-                } else{
-                next[i][j] = state;
-                }
+function setupLife(life) {
+    let html = '';
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (life[i][j] === 0) {
+                html += DEATH_CELL;
+            } else {
+                html += LIVE_CELL;
             }
         }
 
     }
-    lifes = next;
+    return html;
 }
 
-// Đếm hàng xóm
-function countLiveNeighbors(lifes,x,y) {
-    let sum =0;
-    for (let i = -1; i < 2; i++) {
-        for (let j = -1; j < 2; j++) {
-            let col = x+i;
-            let row = y+j;
-            sum += lifes[col][row];
+function myNeighbor(oldLive, i, j) {
+    let numberOfNeighbor = 0;
+    let len = 10;
+    let topAxis = i - 1 >= 0 ? i - 1 : 9;
+    let botAxis = i + 1 < len ? i + 1 : 0;
+    let leftAxis = j - 1 >= 0 ? j - 1 : 9;
+    let rightAxis = j + 1 < len ? j + 1 : 0;
+
+    numberOfNeighbor
+        = oldLive[topAxis][leftAxis] + oldLive[topAxis][j] + oldLive[topAxis][rightAxis]
+        + oldLive[i][leftAxis] + oldLive[i][rightAxis]
+        + oldLive[botAxis][leftAxis] + oldLive[botAxis][j] + oldLive[botAxis][rightAxis];
+
+    return numberOfNeighbor;
+}
+
+function isDeath(me) {
+    return me === DEATH;
+}
+
+function isLive(me) {
+    return me === LIVE;
+}
+
+function nextTheLive(oldLive) {
+    let nextLive = setupLifeData();
+
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let me = oldLive[i][j];
+            let numberOfNeighbor = myNeighbor(oldLive, i, j);
+            if (isLive(me) && (numberOfNeighbor === 2 || numberOfNeighbor === 3)) {
+                nextLive[i][j] = LIVE;
+            } else if (isDeath(me) && numberOfNeighbor === 3) {
+                nextLive[i][j] = LIVE;
+            } else {
+                nextLive[i][j] = DEATH;
+            }
         }
     }
-    sum -= lifes[x][y];
-    return sum;
+    return nextLive;
 }
+
+$(document).ready(function () {
+    let lifeData = setupLifeData();
+    document.getElementById("demo").innerHTML = setupLife(lifeData);
+
+    setInterval(function () {
+        let oldLife = lifeData;
+        let newLife = nextTheLive(oldLife);
+        lifeData = newLife;
+        document.getElementById("demo").innerHTML = setupLife(lifeData);
+    }, 1000)
+});
+
 
 
